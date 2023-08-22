a1064120b4ebae6aa482fe8f022e9ac08106bba8
jakubfiglak <jakub.figlak@netguru.pl>
2023-08-22 12:01:15 +0200

    feat: example

---
+++ web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,155 @@
+import { Link, routes, useParams } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const HomePage = () => {
+  const params = useParams()
+
+  return (
+    <>
+      <MetaTags title="Home" description="Home page" />
+
+      <div className="container">
+        <h1>Scroll issue reproduction</h1>
+
+        <section>
+          <h2>Long paragraph</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            explicabo soluta amet dicta delectus deleniti repellat maxime
+            recusandae facere optio praesentium, ducimus saepe modi placeat
+            mollitia expedita provident aperiam, ab perspiciatis sequi
+            repudiandae nulla tempore similique hic! Ratione amet deserunt, ad
+            ipsum atque doloribus nisi voluptate nostrum consectetur dicta, quas
+            maxime cumque sequi provident corrupti? Neque officiis odio,
+            assumenda, placeat hic aliquid ab natus harum nesciunt eveniet dolor
+            repudiandae fugiat molestiae molestias rem. Molestias reiciendis
+            quis quae eius officiis enim quidem laboriosam vero alias natus?
+            Corporis eaque sunt quidem sapiente accusantium suscipit fuga, quod
+            cumque dicta commodi in iure sed explicabo! Inventore beatae unde
+            cupiditate consectetur ducimus vitae nam eum quod quis. Laboriosam
+            maxime iusto eaque neque corporis temporibus molestiae, saepe
+            ratione accusantium id aperiam facere impedit facilis quidem,
+            suscipit reiciendis explicabo soluta voluptates minima voluptas
+            nulla excepturi nesciunt a molestias. Saepe eos ipsa neque adipisci
+            quae nostrum. Quos repellat voluptates vero rem dolores natus sint,
+            ut delectus excepturi ullam ipsa assumenda! Nulla quod modi sed
+            delectus eligendi provident atque, cumque numquam iste eum nihil ut
+            sint animi. Iste error ab expedita quos laboriosam id quaerat magni
+            consequuntur ad odit eos, asperiores eum quia exercitationem
+            perferendis autem perspiciatis numquam modi tempore odio. Expedita,
+            laborum ab! Consequuntur corrupti, iure omnis consequatur, eligendi
+            natus impedit molestias nobis explicabo rem alias? Tempore minus
+            nulla, labore rerum optio enim incidunt aliquid commodi quas iusto
+            distinctio sint doloremque quo officiis corrupti omnis porro!
+            Minima, necessitatibus tempore! Laudantium enim consequatur dolorem
+            reiciendis soluta. Eveniet laudantium a reiciendis praesentium odio
+            voluptas tempore explicabo consectetur ullam quam nemo quibusdam et
+            alias natus animi magnam, ipsa quas nulla adipisci! A officia
+            placeat ratione minus dignissimos voluptatibus non, aut ipsa ipsum
+            ab quae earum dolorum illum aperiam libero ea sunt, reiciendis
+            porro. Ipsum in saepe molestiae nihil hic non possimus fuga, minus
+            quas reprehenderit magni dolore incidunt, natus illum praesentium
+            eum, obcaecati suscipit velit. Ipsam reiciendis adipisci blanditiis!
+            Saepe, consequatur ea culpa eveniet dolor repudiandae ex esse
+            necessitatibus hic accusamus dolores, accusantium ducimus quibusdam
+            adipisci natus sint labore. Harum, ab quidem nobis pariatur
+            voluptatem vitae assumenda amet velit, consequatur porro eum! Maxime
+            debitis blanditiis illum quo, ex alias ad? Veritatis odio dolores,
+            dolorem, eaque sunt at dolore magnam repellendus non quibusdam
+            voluptas sapiente cupiditate libero quasi, atque animi minima
+            corrupti reiciendis? Ipsum, illo minima corporis nemo tempora
+            voluptas dolor omnis, in officia, tenetur rerum maxime laborum
+            corrupti asperiores magni saepe vel. Reiciendis sapiente amet sed,
+            iusto iste architecto necessitatibus dolorum enim harum aperiam
+            ratione neque molestias? Tempora amet ducimus deleniti doloribus
+            omnis voluptate! Debitis laborum dolor praesentium delectus rerum
+            vero, ad unde cumque consectetur? Illo ipsum at natus quae officia
+            officiis repellendus cum blanditiis cupiditate. Autem ut dolore id
+            explicabo doloremque sed voluptas facilis quaerat, omnis
+            exercitationem temporibus, reiciendis mollitia suscipit sunt tempore
+            facere tenetur voluptatibus fugit natus nisi impedit, nostrum at
+            dolores quo. Laborum sint commodi inventore repellat tempore facere
+            nulla obcaecati, voluptates ipsam? Corrupti autem iure, pariatur,
+            fugiat ratione neque beatae rerum vitae quia necessitatibus
+            praesentium ipsum sunt soluta tenetur inventore perspiciatis in
+            error! Ex error, minus excepturi, voluptatem magnam odio numquam
+            doloribus dolores repellendus exercitationem, eaque sint. Blanditiis
+            necessitatibus voluptatibus aspernatur debitis sit iusto deleniti
+            illum nobis veniam fuga. Esse neque provident nihil debitis dicta
+            dolore fugiat! Natus optio quam labore blanditiis, earum tempore
+            distinctio illum cupiditate exercitationem ullam eos, dolore at quas
+            aspernatur praesentium sunt reiciendis! Accusantium excepturi iure
+            harum tempore alias similique incidunt dicta ipsa praesentium
+            tempora voluptas consequuntur, molestias nisi, placeat quae unde
+            nemo ipsam sequi ducimus suscipit molestiae fugit tenetur. Labore
+            reprehenderit odio, nam ab a aperiam! Voluptas, non numquam corrupti
+            rem vitae consequuntur laudantium deleniti facere, sit sapiente
+            tempore optio, voluptates iusto nemo fugit. Nemo rem hic veritatis
+            corporis aspernatur eum vel, quas, sapiente soluta fugiat, maiores
+            nam necessitatibus animi recusandae ea natus ipsam nostrum
+            laudantium neque doloremque vitae? Eius, eos assumenda, dicta,
+            mollitia amet laborum pariatur sapiente nostrum excepturi voluptatum
+            ipsum illo temporibus nobis repellendus? Hic, quasi? Asperiores
+            dolore id, ipsam voluptatem optio doloremque, adipisci accusantium
+            qui obcaecati enim iste quod quisquam maiores praesentium nemo
+            dolorum, eligendi similique. Consectetur, voluptates veniam et nobis
+            nulla esse blanditiis, quisquam harum suscipit ratione laudantium
+            facilis aliquid ipsum magnam labore. Quia molestias illum neque
+            nulla placeat praesentium harum commodi aperiam exercitationem at
+            dolorum vitae, recusandae quaerat assumenda et voluptate tenetur
+            autem. Nemo dolore culpa, quasi incidunt voluptatum nesciunt quis,
+            aperiam recusandae ducimus voluptatibus facere aliquid consequatur
+            rerum. Quae labore asperiores autem deserunt perspiciatis. Alias
+            facere praesentium minima blanditiis optio facilis, esse enim, animi
+            amet cumque ipsam. Odio consectetur molestias numquam dignissimos
+            aperiam veritatis dicta ad tempore esse repellat magnam saepe
+            cupiditate natus officiis ex odit ipsa labore commodi doloremque
+            error, atque a vero nulla? Qui cumque eum ad, cum, laborum, omnis
+            laboriosam suscipit porro inventore natus possimus quasi magnam
+            voluptate ipsum repellendus! Nesciunt laborum qui labore commodi
+            eius soluta ullam, impedit et est iusto ut eum earum. Labore aliquid
+            culpa dolore voluptas cupiditate dolorum ipsam, minus architecto? Ab
+            consectetur molestias amet vel excepturi impedit, obcaecati
+            voluptatibus! Repellendus possimus omnis, beatae magnam quos eos
+            animi error quaerat necessitatibus ab quibusdam fugiat nemo.
+            Deleniti repudiandae sequi labore quo nostrum tenetur impedit facere
+            tempore. Tempore enim voluptas nam hic architecto assumenda quaerat,
+            est natus delectus? Similique veritatis cumque aperiam laboriosam
+            deserunt sequi pariatur natus nobis quae esse voluptate et, sapiente
+            modi nihil eos, vel, omnis tempore? Vero, atque, quibusdam rerum
+            inventore eligendi explicabo numquam perspiciatis ea voluptatibus
+            quas tempora incidunt et dolores corrupti quo ex, reprehenderit
+            voluptas vel officia officiis? Consectetur dolor, ab incidunt
+            placeat sunt modi, amet molestiae ea dolore aspernatur quas, ipsam
+            optio suscipit quidem perspiciatis. Porro quia, consectetur magnam
+            rerum in dolore facilis ducimus at impedit unde voluptate minus
+            odit. Voluptatum beatae iusto tenetur expedita dignissimos, fugit
+            eligendi voluptate, officia saepe totam hic. Ipsum, consectetur
+            nesciunt iste molestias ex delectus similique asperiores totam
+            tenetur exercitationem voluptatum aliquam possimus vitae aperiam
+            enim quas error tempora numquam? Ad, tenetur ullam praesentium, quis
+            voluptate doloribus eveniet voluptates modi eaque rerum molestiae
+            iste dolor dolorum similique. Illo, ex perspiciatis.
+          </p>
+        </section>
+
+        <section>
+          <h2>Set query parameters</h2>
+          <div className="links-container">
+            {['option_one', 'option_two', 'option_three'].map((option) => (
+              <Link key={option} to={routes.home({ ...params, test: option })}>
+                {option}
+              </Link>
+            ))}
+          </div>
+          <div>
+            <h4>Current params:</h4>
+            <pre>{JSON.stringify(params, null, 2)}</pre>
+          </div>
+        </section>
+      </div>
+    </>
+  )
+}
+
+export default HomePage
